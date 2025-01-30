df8acfe404931ebe6ef10a1210b439b277590f20
MasoudAbedi <Masoud.ab.ir@gmail.com>
2025-01-30 16:48:03 +0100

    Updated java code.

--- app/assets/javascripts/mds_resources.js
+++ app/assets/javascripts/mds_resources.js
@@ -1,26 +1,22 @@
-function hide_field() {
-        //resource = serialize()
-        check_ID = $j('input[name="project[extended_metadata_attributes][data][resource_identifier_Project]"]').val();
-
-
-
-        if (check_ID == '') {
-            //$j('select[name="studyhub_resource[custom_metadata_attributes][data][resource_use_rights_label]"]').val('Not applicable');
-            //$j('input:text[name^="project[extended_metadata_attributes][data][resource_identifier_Project]"]').parent().parent().hide();
-            $("input:text[name='project[extended_metadata_attributes][data][Resource_identifier_Project]']").parent().hide();
-
-        }
-        else {
-            $("input:text[name='project[extended_metadata_attributes][data][Resource_identifier_Project]']").parent().hide();
-            $("label").text(function(index, currenText){return currenText+':'+value;});
-        }
-
-        /*if (check_license.startsWith('CC')) {
-            $j('input:radio[name^="studyhub_resource[custom_metadata_attributes][data][resource_use_rights_authors_confirmation"]').parent().parent().show();
-            $j('input:radio[name="studyhub_resource[custom_metadata_attributes][data][resource_use_rights_support_by_licencing]"]').parent().parent().show();
-        }else {
-            $j('input:radio[name^="studyhub_resource[custom_metadata_attributes][data][resource_use_rights_authors_confirmation"]').parent().parent().hide();
-            $j('input:radio[name="studyhub_resource[custom_metadata_attributes][data][resource_use_rights_support_by_licencing]"]').parent().parent().hide();
-        }*/
-
+var MDS = {
+    hide_field: function() {
+        $j('.form-group').each(function () {
+            var title = $j(this).data('attribute-title');
+            var value = $j(this).data('attribute-value');
+            if (title === "Resource_identifier_Project" && (value === "" || value === null )) {
+                $j(this).hide();
+                //$j(this).find('input').prop('disabled', true);//, select, textarea
+            }
+        });
     }
+}
+
+
+$j(document).ready(function () {
+    MDS.hide_field();
+});
+
+
+$j(document).on('change keyup', '#extended_metadata_attributes_extended_metadata_type_id', function () {
+    MDS.hide_field();
+});
