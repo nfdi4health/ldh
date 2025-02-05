be77be69205ebb29c039f74a8e410f109e685cb4
MasoudAbedi <Masoud.ab.ir@gmail.com>
2025-02-05 15:01:45 +0100

    Updated java code.

--- app/assets/javascripts/mds_resources.js
+++ app/assets/javascripts/mds_resources.js
@@ -3,20 +3,43 @@ var MDS = {
         $j('.form-group').each(function () {
             var title = $j(this).data('attribute-title');
             var value = $j(this).data('attribute-value');
-            if (title === "Resource_identifier_Project" && (value === "" || value === null )) {
+            var all_values_json = $j(this).data('attribute-all');
+
+            if (title === "Resource_identifier_Project" && (value === "" || value === null)) {
                 $j(this).hide();
-                //$j(this).find('input').prop('disabled', true);//, select, textarea
+            }
+            else
+            {
+                $j(this).show();
+            }
+
+            try {
+                var all_values = JSON.parse(all_values_json.json_metadata);
+
+                if (title === "Design_Project" && (value === "" || value === null))
+                {
+                    if (all_values["Resource_classification_Project"] &&
+                        !(all_values["Resource_classification_Project"]["Resource_classification_type_Project"] === "Study" || all_values["Resource_classification_Project"]["Resource_classification_type_Project"] === "Substudy"
+                            || all_values["Resource_classification_Project"]["Resource_classification_type_Project"] === "Registry"|| all_values["Resource_classification_Project"]["Resource_classification_type_Project"] === "Secondary data source"))
+                    {
+                        $j(this).hide();
+                    }
+                    else
+                    {
+                        $j(this).show();
+                    }
+                }
+            } catch (error) {
+                console.info("JSON Parse Error:", error);
             }
         });
     }
-}
+};
 
 
-$j(document).ready(function () {
-    MDS.hide_field();
-});
 
 
-$j(document).on('change keyup', '#extended_metadata_attributes_extended_metadata_type_id', function () {
-    MDS.hide_field();
-});
+
+
+
+
