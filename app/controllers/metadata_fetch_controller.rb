8436d73ea5de3062778664b3d3e0f1da228ad39e
René Hänsel <rene.haensel@gmx.de>
2025-07-09 18:27:36 +0200

    update code prefill MDS

--- app/controllers/metadata_fetch_controller.rb
+++ app/controllers/metadata_fetch_controller.rb
@@ -7,9 +7,9 @@ class MetadataFetchController < ApplicationController
     resource_id = params[:search]
     source = params[:options]
 
-    @metadata = fetch_from_gchsh(resource_id)
+    @metadata = fetch_metadata_from_source(resource_id, source)
     if @metadata
-      flash[:notice] = "Metadata fetched from health study hub with ID #{resource_id}"
+      flash[:notice] = "Metadata fetched from #{source} with ID #{resource_id}"
     else
       flash[:alert] = "No data found from #{resource_id}"
     end
@@ -24,10 +24,27 @@ class MetadataFetchController < ApplicationController
     #render :show
   end
 
+  private
 
-def fetch_from_gchsh(resource_id)
-    url = "https://health-study-hub.de/api/resource/#{resource_id}"
-    get_json_from_url(url)
+  def fetch_metadata_from_source(resource_id, source)
+    case source
+    when "GCHSH"
+      fetch_from_gchsh(resource_id)
+    when "clinical.gov"
+      fetch_from_clinical_gov(resource_id)
+    else
+      nil
+    end
+  end
+
+  def fetch_from_gchsh(resource_id)
+      url = "https://health-study-hub.de/api/resource/#{resource_id}"
+      get_json_from_url(url)
+  end
+
+  def fetch_from_clinical_gov(resource_id)
+      url = "https://clinicaltrials.gov/api/v2/studies/#{resource_id}?format=json"
+      get_json_from_url(url)
   end
 
 
